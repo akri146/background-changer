88c0a14b348f680d1969bf0db385f58955041781
Александр <metacrit46@gmail.com>
2025-04-10 20:27:05 +0300

    1

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const btn = document.getElementById('btn');
 const text = document.getElementById('color');
 const bodyColor = document.getElementById('body');
 
-const colors = ["red", "green", "blue"];
-
 btn.addEventListener('click', function(){
-	bodyColor.style.backgroundColor = generateRandomHex();
-	text.innerText = bodyColor.style.backgroundColor;
+	const randomColor = generateRandomHex();
+	bodyColor.style.backgroundColor = randomColor;
+	text.textContent = randomColor;
 })
 
 function generateRandomHex() {
 	return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
 }
+
+bodyColor.style.backgroundColor = '#ffffff';
+text.textContent = '#ffffff';
